a590027c0d0b2ed010b4cb777ab9b78883ad67a8
Naoki Takezoe <takezoe@gmail.com>
2022-08-28 03:46:06 +0900

    Fixup

--- src/main/java/com/github/takezoe/tomcat/Runner.java
+++ src/main/java/com/github/takezoe/tomcat/Runner.java
@@ -7,30 +7,21 @@ public class Runner
 {
     public static void main(String[] args) throws Exception
     {
-        System.out.println("===============================");
-        int port = 8080;
-        String webappDirLocation = "";
-        for(int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
+        Tomcat tomcat = new Tomcat();
+
         for(int i = 0; i < args.length; i++) {
             if (args[i].equals("--port") && args.length > i + 1) {
-                port = Integer.parseInt(args[i + 1]);
-                i++;
+                int port = Integer.parseInt(args[i + 1]);
+                tomcat.setPort(port);
+                i = i + 1;
             }
             else {
-                webappDirLocation = args[i];
+                String context = args[i + 1];
+                tomcat.addWebapp("/", new File(context).getAbsolutePath());
             }
         }
-        System.out.println("===============================");
 
-        Tomcat tomcat = new Tomcat();
-        System.out.println("configuring port: " + port);
-        tomcat.setPort(port);
         tomcat.getConnector();
-
-        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
-
         tomcat.start();
         tomcat.getServer().await();
     }
